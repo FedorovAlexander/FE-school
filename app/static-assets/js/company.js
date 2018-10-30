340594d1b1c09a3ebd52a1ddec300aaaa5c6d2d4
alex <hamster2240@gmail.com>
2018-10-30 14:39:45 +0300

    Import img links on company page

--- app/static-assets/js/company.js
+++ app/static-assets/js/company.js
@@ -6,8 +6,10 @@ fetch('../../rest/vacancies')
     window.onload = function() {
       const companyName = document.querySelector('.company-info__title');
       const companyDescription = document.querySelector('.company-info__description');
+      const imgCompany = document.querySelector('.company-img')
       let posObj = JSON.parse(sessionStorage.getItem('positionObj'));
       companyName.innerText = posObj.companyName;
       companyDescription.innerText = posObj.description;
+      imgCompany.src = posObj.imgLink;
     }
   })
