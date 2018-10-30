4c01e86f1bd7c6494e0522768734251be5c8da1a
alex <hamster2240@gmail.com>
2018-10-30 10:10:17 +0300

    Add the chart icon animation

--- app/static-assets/js/index.js
+++ app/static-assets/js/index.js
@@ -3,3 +3,29 @@ if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
      let img = document.querySelector('.page-header__img')
      img.style.top = '0.12rem';
 }
+
+function changeBarIcon() {
+  let chartLink = document.querySelector('.page-header__chart-link-block')
+  let bar1 = document.querySelector('#bar1')
+  let bar2 = document.querySelector('#bar2')
+  let bar3 = document.querySelector('#bar3')
+  chartLink.addEventListener('mouseenter', function() {
+    bar1.style.y = '50';
+    bar1.style.transition = 'y 0.1s linear';
+    bar2.style.y = '200';
+    bar2.style.transition = 'y 0.1s linear';
+    bar3.style.y = '0';
+    bar3.style.height = '600';
+    bar3.style.transition = 'y 0.1s linear';
+  })
+  chartLink.addEventListener('mouseleave', function() {
+    bar1.style.y = '162.2';
+    bar1.style.transition = 'y 0.1s linear';
+    bar2.style.y = '0';
+    bar2.style.transition = 'y 0.1s linear';
+    bar3.style.y = '82.4';
+    bar3.style.height = '500px';
+    bar3.style.transition = 'y 0.1s linear';
+  })
+}
+changeBarIcon()
